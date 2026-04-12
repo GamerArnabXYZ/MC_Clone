c64da873fcbc1b1531aafa58be0bf1352f3691ea
GamerArnabXYZ <arnabdasdas2010@gmail.com>
2026-04-12 16:03:23 +0600

    Test Build

--- src/main_impl.h
+++ src/main_impl.h
@@ -12,48 +12,20 @@ Multiplayer, login, and launcher completely removed.
 #include "Utils.h"
 #include "Options.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-
-static void start_logging(void) {
-	const char* path = "/sdcard/Android/data/com.voxelcraft.android.client/files/log.txt";
-	remove(path);
-	if (freopen(path, "w", stdout) == NULL) return;
-	freopen(path, "a", stderr);
-	setvbuf(stdout, NULL, _IONBF, 0);
-	setvbuf(stderr, NULL, _IONBF, 0);
-	printf("=== VoxelCraft Mobile Debugger ===\n");
-	fflush(stdout);
-}
-
 static void SetupProgram(int argc, char** argv) {
-	start_logging();
-	printf("[1] Logger init\n"); fflush(stdout);
 	Logger_WarnFunc = Logger_DialogWarn;
-
-	printf("[2] Platform_Init\n"); fflush(stdout);
 	Platform_Init();
-
-	printf("[3] SetDefaultCurrentDirectory\n"); fflush(stdout);
 	Platform_SetDefaultCurrentDirectory(argc, argv);
-
-	printf("[4] Window_PreInit\n"); fflush(stdout);
 	Window_PreInit();
-
-	printf("[5] Window_Init\n"); fflush(stdout);
 	Window_Init();
-
-	printf("[6] Options_Load\n"); fflush(stdout);
 	Options_Load();
-
-	printf("[7] SetupProgram done\n"); fflush(stdout);
+	/* Set default username if not saved */
+	if (!Game_Username.length)
+		String_AppendConst(&Game_Username, "VoxelPlayer");
 }
 
 static void RunGame(void) {
-	printf("[8] Game_Setup start\n"); fflush(stdout);
 	Game_Setup();
-	printf("[9] Game loop start\n"); fflush(stdout);
 	while (Game_Running) { Game_RenderFrame(); }
 	Game_Free();
 	Window_Destroy();
